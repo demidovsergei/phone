ed5430097d5009240f3f14991315b23259e87033
demidovsergei <sergeidemidov97@yandex.ru>
2023-03-15 19:21:45 +0300

    main changed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int nashsumm = 100;
-        int popolnenie = 1100;
+        int initial_amount = 100;
+        int replenishment = 1100;
         int start = 1000;
         int bonus;
-        int stopsumm;
-        if (popolnenie > start) {
-            bonus = popolnenie / 100;
-            stopsumm = nashsumm + popolnenie + bonus;
-            System.out.println(stopsumm);
+        int final_amount;
+        if (replenishment > start) {
+            bonus = replenishment / 100;
+            final_amount = initial_amount + replenishment + bonus;
+            System.out.println(final_amount);
         } else {
             bonus = 0;
         }
