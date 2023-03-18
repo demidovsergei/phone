febf1c8679138cdca13ee81f5cb34e59a3d65de6
demidovsergei <124704208+demidovsergei@users.noreply.github.com>
2023-03-18 10:12:18 +0300

    Changed main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int initialAmount = 100;
-        int replenishment = 1100;
+        int replenishment = 100;
         int start = 1000;
         int bonus;
         int finalAmount;
         if (replenishment > start) {
             bonus = replenishment / 100;
             finalAmount = initialAmount + replenishment + bonus;
-            System.out.println(finalAmount);
         } else {
+            finalAmount = initialAmount + replenishment;
             bonus = 0;
         }
-
+        System.out.println(finalAmount);
         System.out.println(bonus);
     }
 }
